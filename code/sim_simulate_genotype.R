a445bd09c6d941fad580bac07747c225a33fbe7c
Yanyu Liang <yanyul@uchicago.edu>
2019-12-03 09:23:56 -0600

    added simulation pipeline for single snp case

---
+++ code/sim_simulate_genotype.R
@@ -0,0 +1,37 @@
+library(optparse)
+
+option_list <- list(
+    make_option(c("-p", "--param"), type="character", default=NULL,
+                help="input YAML telling the parameters of simulation",
+                metavar="character"),
+    make_option(c("-o", "--output_prefix"), type="character", default=NULL,
+                help="prefix of output",
+                metavar="character"),
+    make_option(c("-n", "--nreplicate"), type="numeric", default=NULL,
+                help="number of replicates",
+                metavar="character")
+)
+
+opt_parser <- OptionParser(option_list=option_list)
+opt <- parse_args(opt_parser)
+
+library(mixqtl)
+
+## load parameter
+param = yaml::read_yaml(opt$param)
+set.seed(param$seed)
+
+## Sample size and number of replications
+N = param$Nsample  # number of individuals
+M = opt$nreplicate  # number of replicates (fixing gene characteristics and change genetic effect)
+
+## Pre-fixed parameters
+maf = c(param$maf_l, param$maf_h)  # MAF range of all variants in the simulation
+
+# Simulate genotype
+out = create_genotype(maf, N, M)
+
+# Save
+for(m in 1 : M) {  # iterate over replicates
+  saveRDS(out[[m]], paste0(opt$output_prefix, m, '.rds'))
+}
---
+++ code/sim_simulate_read_count.R
@@ -0,0 +1,46 @@
+library(optparse)
+
+option_list <- list(
+    make_option(c("-p", "--param"), type="character", default=NULL,
+                help="input YAML telling the parameters of simulation",
+                metavar="character"),
+    make_option(c("-o", "--output_prefix"), type="character", default=NULL,
+                help="prefix of output",
+                metavar="character"),
+    make_option(c("-y", "--genotype"), type="character", default=NULL,
+                help="simulated gene file",
+                metavar="character"),
+    make_option(c("-g", "--gene"), type="character", default=NULL,
+                help="simulated genotype file",
+                metavar="character")
+)
+
+opt_parser <- OptionParser(option_list=option_list)
+opt <- parse_args(opt_parser)
+
+library(mixqtl)
+
+## load parameter
+param = yaml::read_yaml(opt$param)
+set.seed(param$seed)
+
+## Pre-fixed parameters
+L_read = param$L_read
+betas = log(eval(parse(text = param$betas)))  # effect size (log fold change)
+
+# load the gene
+gene_param = readRDS(opt$gene)
+gene = create_gene(gene_param$L_gene, gene_param$library_dist, gene_param$theta_dist, gene_param$f, gene_param$maf, gene_param$p)
+
+# load genotypes
+genotype = readRDS(opt$genotype)
+fG = genotype$fG
+geno = genotype$genotype
+
+# Simulate reads
+data_collector = list()
+for(beta in betas) {
+  df = simulate_read_count(gene, geno, beta, L_read, param$y_dist)
+  data_collector[[length(data_collector) + 1]] = list(data = df, fG = fG, beta = beta)
+}
+saveRDS(data_collector, opt$output)
