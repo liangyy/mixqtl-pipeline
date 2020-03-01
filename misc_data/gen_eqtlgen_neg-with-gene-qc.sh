b93e3fc3872e47c4cca3df1cbf41ab0ace5e181a
Yanyu Liang <yanyul@uchicago.edu>
2020-03-01 15:21:23 -0600

    added eqtlgen ciseqtl list gen script

---
+++ misc_data/gen_eqtlgen_neg-with-gene-qc.sh
@@ -0,0 +1,22 @@
+# ARGV:
+# 1. size of sub-sample
+# 2. gene list
+
+prefix=eqtlgen_neg
+genelist=gtex-v8-genes-passed-qc_gene_list.txt  # $2
+
+
+if [ ! -f $prefix.pval_gt_0.5.txt.gz ]; then
+  zcat /gpfs/data/im-lab/nas40t2/yanyul/eQTLGen/cis-eQTLs_full_20180905_with_GTExV8ID.txt.gz | awk '{split($1,b,"E"); if($1>0.5 && b[2]=="") print $0}' | gzip > $prefix.pval_gt_0.5.txt.gz
+fi
+
+if [ ! -f $prefix.pval_gt_0.5-with-gene-qc.txt.gz ];then
+  awk 'FNR==NR{a[$1]=1;next}{ if($8 in a) print $0}' $genelist <(zcat $prefix.pval_gt_0.5.txt.gz) | gzip > $prefix.pval_gt_0.5-with-gene-qc.txt.gz
+fi
+
+nrow=`zcat $prefix.pval_gt_0.5-with-gene-qc.txt.gz | wc -l`
+
+Rscript subsample_idx.R --number_total $nrow --number_sub $1 --output $prefix.idx.subsample$1-with-gene-qc.txt
+
+awk 'FNR==NR{a[$1]=1;next}{ if(FNR in a) print $0}' $prefix.idx.subsample$1-with-gene-qc.txt <(zcat $prefix.pval_gt_0.5-with-gene-qc.txt.gz) | gzip > $prefix.subsample$1-with-gene-qc.txt.gz
+
