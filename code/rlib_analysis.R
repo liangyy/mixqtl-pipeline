2a79e58a753b1b2fafdaaac1834111d2bde06d0f
Yanyu Liang <yanyul@uchicago.edu>
2020-03-01 17:50:29 -0600

    added results on mixqtl gtex v8 whole blood

--- code/rlib_analysis.R
+++ code/rlib_analysis.R
@@ -24,3 +24,15 @@ inclusion_curve = function(is_true, first_n = 50) {
   is_true_first = is_true[1 : first_n]
   return(data.frame(number_of_signals = 1 : length(is_true_first), inclusion_rate = cumsum(is_true_first) / nsig))
 }
+
+trim_dot = function(str) {
+  unlist(lapply(strsplit(as.character(str), '\\.'), function(x) { x[1] }))
+}
+
+my_qqplot = function(pval, group) {
+  df = data.frame(pval = pval, grp = group)
+  df = df %>% group_by(grp) %>% mutate(pval_obs = pval[order(pval)], pval_exp = (1 : length(pval)) / length(pval)) %>% ungroup()
+  df$pval_obs[df$pval_obs < 1e-50] = 1e-50
+  p = df %>% ggplot() + geom_point(aes(x = -log10(pval_exp), y = -log10(pval_obs), color = grp))
+  p
+}
