8126390ff3e17cd01dbfe0a901f4d0fccd50f354
Yanyu Liang <yanyul@uchicago.edu>
2020-03-03 09:46:13 -0600

    added extract script for finemap

--- misc_scripts/gtex_v8_postprocess/extract.sh
+++ misc_scripts/gtex_v8_postprocess/extract.sh
@@ -17,7 +17,11 @@ output=$7
 
 zcat $fromm | head -n 1 > $output.temp
 
-awk -v TGENE=$targetgene -v TVAR=$targetvar -v FGENE=$frommgene -v FVAR=$frommvar 'FNR==NR{a[$(TGENE)"-"$(TVAR)]=1;next}{split($(FGENE),b,"."); if(b[1]"-"$(FVAR) in a) print $0}' <(zcat $target) <(zcat $fromm) >> $output.temp
-
+if [[ $targetgene != "-1" && $frommgene != "-1" ]]
+then
+  awk -v TGENE=$targetgene -v TVAR=$targetvar -v FGENE=$frommgene -v FVAR=$frommvar 'FNR==NR{a[$(TGENE)"-"$(TVAR)]=1;next}{split($(FGENE),b,"."); if(b[1]"-"$(FVAR) in a) print $0}' <(zcat $target) <(zcat $fromm) >> $output.temp
+else
+  awk -v TVAR=$targetvar -v FVAR=$frommvar 'FNR==NR{a[$(TVAR)]=1;next}{if($(FVAR) in a) print $0}' <(zcat $target) <(zcat $fromm) >> $output.temp
+fi
 cat $output.temp | gzip > $output
 rm $output.temp
---
+++ qsub/gtex_v8/postprocess_extract/submit_finemap.sh
@@ -0,0 +1,4 @@
+qsub -v TARGET=mapped-hepg2.sign.id.LP190708,FROM=Whole_Blood-x-result_pip-mixfine run_finemap.qsub
+qsub -v TARGET=mapped-hepg2.sign.id.LP190708,FROM=Whole_Blood-x-result_pip-nefine run_finemap.qsub
+qsub -v TARGET=mapped-k562.sign.id.LP190708,FROM=Whole_Blood-x-result_pip-mixfine run_finemap.qsub
+qsub -v TARGET=mapped-k562.sign.id.LP190708,FROM=Whole_Blood-x-result_pip-nefine run_finemap.qsub
