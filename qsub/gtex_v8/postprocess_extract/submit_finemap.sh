3c9b0bc7f1fd416f70644fd210edc24a5f03989b
Yanyu Liang <yanyul@uchicago.edu>
2020-03-03 21:15:33 -0600

    minor change

--- qsub/gtex_v8/postprocess_extract/submit_finemap.sh
+++ qsub/gtex_v8/postprocess_extract/submit_finemap.sh
@@ -1,4 +1,2 @@
-qsub -v TARGET=mapped-hepg2.sign.id.LP190708,FROM=Whole_Blood-x-result_pip-mixfine run_finemap.qsub
-qsub -v TARGET=mapped-hepg2.sign.id.LP190708,FROM=Whole_Blood-x-result_pip-nefine run_finemap.qsub
-qsub -v TARGET=mapped-k562.sign.id.LP190708,FROM=Whole_Blood-x-result_pip-mixfine run_finemap.qsub
-qsub -v TARGET=mapped-k562.sign.id.LP190708,FROM=Whole_Blood-x-result_pip-nefine run_finemap.qsub
+qsub -v TARGET=mapped-SuRE_SNP_table_LP190708,FROM=Whole_Blood-x-result_pip-mixfine run_finemap.qsub
+qsub -v TARGET=mapped-SuRE_SNP_table_LP190708,FROM=Whole_Blood-x-result_pip-nefine run_finemap.qsub
