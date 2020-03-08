233a4e30828a9556ce6618bef2f0e3080294bc65
Yanyu Liang <yanyul@uchicago.edu>
2020-03-08 12:27:31 -0500

    added pack up for mixfine

--- qsub/gtex_v8/mixfine-subsample/submit_pack.sh
+++ qsub/gtex_v8/mixfine-subsample/submit_pack.sh
@@ -4,19 +4,22 @@
 # ARGV4: name of pack 
 # ARGV5: folder of result (after merging, not neccssary to merge though)
 
-MYCONFIGS=$1
-genelist=$2
-SUBSETDIR=$3
-packname=$4
-outdir=$5
+MYCONFIGS=config.mixfine.yaml:config.nefine.yaml  # $1
+TASKNAME=gene_list_whole_blood_for_subsampling_with_qc  
+# genelist=$2
+SUBSETDIR=subsample_whole_blood  # $3
+packname=mypack-03-08-20  # $4
+outdir=/scratch/t.cri.yliang/mixqtl-pipeline-results/gtex_v8-results/mixfine-subsample  # $5
 
 if [ ! -d "pack_logs" ]; then
   mkdir pack_logs/
 fi
 
+for genelist in `ls gene_list/$TASKNAME* | sed 's#gene_list/##g'`;
+do
+  echo qsub -v GENELIST=$genelist,PACKNAME=$packname,MYCONFIGS=$MYCONFIGS,OUTDIR=$outdir,SUBSETDIR=$SUBSETDIR -N $packname run_pack.qsub
+done
 
-
-qsub -v GENELIST=qsub/mixfine/whole_blood/$genelist,PACKNAME=$packname,MYCONFIGS=$MYCONFIGS,OUTDIR=$outdir,SUBSETDIR=$SUBSETDIR -N $packname run_pack.qsub
 
 
 
