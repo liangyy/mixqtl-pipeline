bd9c5f699a3c89f83b46ff773cf76ed70c1af92f
Yanyu Liang <yanyul@uchicago.edu>
2020-04-13 22:11:23 -0500

    added a low count gene list

---
+++ misc_data/gen_gene_low_count.R
@@ -0,0 +1,15 @@
+library(dplyr)
+
+dl = readRDS('/scratch/t.cri.yliang/mixqtl-pipeline-results/gtex_v8-preprocess/expression/merge-Whole_Blood.rds')
+trc_cutoff = 50
+trc_nobs_cutoff = 670
+
+trc = rowSums(dl$df_trc <= trc_cutoff)
+
+df = data.frame(n_low_trc = trc, gene = rownames(dl$df_trc))
+
+df = df %>% mutate(pass_trc_low = n_low_trc >= trc_nobs_cutoff)
+
+write.table(df %>% filter(pass_trc_low) %>% select(gene), 'gtex-v8-genes-low-expr_gene_list.txt', row = F, col = F, quo = F, sep = '\t')
+
+
